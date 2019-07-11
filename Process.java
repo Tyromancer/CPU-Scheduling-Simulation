2de2f0212d946c5b6c165e724af9ffbd04f2ec73
Tyromancer <yangb7@rpi.edu>
2019-07-11 15:55:33 -0400

    Update rand and process

---
+++ Process.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+
+public class Process {
+    private String id;
+    private int turnAroundTime;
+    private int cpuBurst;
+    private int waitTime;
+
+    public Process(String id) {
+        this.id = id;
+        // TODO
+    }
+
+
+}
---
+++ Rand48.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public final class Rand48 {
+
+    private static final long MODULO = (1L << 48);
+
+    private long gen;
+
+    public Rand48() {
+        setSeed(System.currentTimeMillis());
+    }
+
+    public void setSeed(long seed) {
+        gen = ((seed << 16) | 0x330eL) & (MODULO - 1);
+    }
+
+    private long next() {
+        gen = (0x5deece66dL * gen + 0xbL) & (MODULO - 1);
+        return gen;
+    }
+
+    public double nextDouble() {
+        return (double) next() / MODULO;
+    }
+
+    public static void main(String[] args) {
+        Rand48 rand = new Rand48();
+        rand.setSeed(47L);
+        for(int i = 0; i < 20; i++) {
+            System.out.format("%.16f\n", rand.nextDouble());
+        }
+    }
+}
