7b5b98fce3e82499095493ffaf3a214428364aaf
Tyromancer <yangb7@rpi.edu>
2019-07-11 19:32:45 +0000

    Update java file

--- Project.java
+++ Project.java
@@ -11,19 +11,52 @@ public class Project {
 
 	long seed;
 	try {
-		seed = Long.parseLong(argv[1]);
+		seed = Long.parseLong(argv\s[1]);
 	} catch (NumberFormatException e) {
-		System.out.println("Invalid input for seed");
+		System.out.println("ERROR: Invalid input for seed " + e.getMessage());
 		return;
 	}
 
 	double lamb;
 	try {
-		lamb = Double.parseDouble(argv[2]);
+		lamb = Double.parseDouble(args[2]);
 	} catch (NumberFormatException e) {
-		System.out.println("Invalid input for lambda value");
+		System.out.println("ERROR: Invalid input for lambda value " + e.getMessage());
 		return;
 	}
+	
+	int upperBound;
+	try {
+		upperBound = Integer.parseInt(args[3]);
+		if (upperBound <= 0) {
+			throw new NumberFormatException("Value too small");
+		}
+	} catch (NumberFormatException e) {
+		System.out.println("ERROR: Invalid input for upper bound value " + e.getMessage());
+	}
+
+	int numProcess;
+	try {
+		numProcess = Integer.parseInt(args[4]);
+		if (numProcess <= 0) {
+			throw new NumberFormatException("Value too small");
+		}
+	} catch (NumberFormatException e) {
+		System.out.println("ERROR: invalid input for number of total processes");
+		return;
+	}
+
+	int timeSwitch;
+	try {
+		timeSwitch = Integer.parseInt(args[5]);
+		if (timeSwitch < 0) {
+			throw new NumberFormatException("Value too small");
+		}
+	} catch (NumberFormatException e) {
+		System.out.println("ERROR: invalid input for time cost of context switch " + e.getMessage());
+	}
+
+
 
 
     }
