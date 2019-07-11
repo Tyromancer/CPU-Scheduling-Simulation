c9e2967d94665a93fa36813a20c7a4a67b7a4185
Sun Yutao <sunyu.tt@163.com>
2019-07-11 17:23:07 -0400

    rua

--- Process.java
+++ Process.java
@@ -2,9 +2,11 @@ import java.util.*;
 import java.lang.*;
 
 public class Process {
+	//public static final String 
 	public static final String READY = "READY";
 	public static final String RUNNING = "RUNNING";
 	public static final String BLOCKED = "BLOCKED";
+	public static final String ENDED = "ENDED";
 	
     private String id;
     private int turnAroundTime;
@@ -14,19 +16,25 @@ public class Process {
     private int burstNum;
     private String status;
     private int remainTime;
+    private int[] burstTimes;
+    private int[] ioTimes;
 
     public Process(String id, Rand48 rand, Double lamb) {
         this.id = id;
-        this.arriveTime = (int) (Math.log(rand.nextDouble())*-1/lamb);
+        this.arriveTime = (int) random(rand, lamb);
         this.burstNum = (int) (rand.nextDouble() * 100) + 1;
-        System.out.println("ArriveTime: " + arriveTime);
+        this.burstTimes = new int[burstNum];
+        this.ioTimes = new int[burstNum];
+        
+        for (int i = 0; i < burstNum - 1; i++) {
+			burstTimes[i] = (int) (random(rand, lamb)) + 1;
+			ioTimes[i] = (int) (random(rand, lamb)) + 1;
+		}
+        burstTimes[burstNum - 1] = (int) (random(rand, lamb)) + 1;
+        
+        System.out.println("Process " + id + ": ArriveTime: " + arriveTime);
         System.out.println("BurstNum: " + burstNum);
         // TODO
-    }
-    
-    public Process(Process p)
-    {
-    	
     }
 
     public String status()
@@ -37,6 +45,16 @@ public class Process {
     public int remainTime()
     {
     	return this.remainTime;
+    }
+    
+    public void run()
+    {
+    	
+    }
+    
+    private double random(Rand48 rand, Double lamb)
+    {
+    	return Math.log(rand.nextDouble()) * -1 / lamb;
     }
 
 }
